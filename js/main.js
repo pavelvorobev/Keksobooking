50cea6d3f676de901e4a4decad23555ffc380d05
Pavel Vorobev <pavel_9_5@mail.ru>
2021-02-13 18:26:54 +0300

    Добавляет генерацию временных данных

--- js/main.js
+++ js/main.js
@@ -17,6 +17,63 @@ const getRandomFloatInt = (min, max, precision) => {
   throw new Error('getRandomFloatInt - диапазон или точность не соответствуют условию');
 };
 
-getRandomRoundedInt(1, 3);
-getRandomFloatInt(1.2, 1.3, 2);
+const TOTAL_USERS = 8;
+const PRICE = {
+  min: 5000,
+  max: 100000,
+};
+const TYPE = ['palace', 'flat', 'house', 'bungalow'];
+const ROOMS = {
+  min: 1,
+  max: 6,
+};
+const GUESTS = {
+  min: 1,
+  max: 12,
+};
+const CHECKIN_TIME = ['12:00', '13:00', '14:00'];
+const CHECKOUT_TIME = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+                'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+                'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+const getRandomArrayElement = (array) => {
+  return array[getRandomRoundedInt(0, array.length - 1)];
+};
 
+const getRandomArrayElements = (array) => {
+ return array.slice(getRandomRoundedInt(1, array.length - 1));
+};
+
+const createRandomAnnouncement = () => {
+  let locationX = getRandomFloatInt(35.65000, 35.70000, 5);
+  let locationY = getRandomFloatInt(139.70000, 139.80000, 5);
+  return {
+    author: {
+      avatar: `img/avatars/user0${getRandomRoundedInt(1, TOTAL_USERS)}.png`,
+    },
+    location: {
+      x: locationX,
+      y: locationY,
+    },
+    offer: {
+      title: 'Заголовок',
+      address: `${locationX},${locationY}`,
+      price: getRandomRoundedInt(PRICE.min, PRICE.max),
+      type: getRandomArrayElement(TYPE),
+      rooms: getRandomRoundedInt(ROOMS.min, ROOMS.max),
+      guests: getRandomRoundedInt(GUESTS.min, GUESTS.max),
+      checkin: getRandomArrayElement(CHECKIN_TIME),
+      checkout: getRandomArrayElement(CHECKOUT_TIME),
+      features: getRandomArrayElements(FEATURES),
+      description: 'Описание',
+      photos: getRandomArrayElements(PHOTOS),
+    },
+  };
+};
+
+const randomAnnouncements = new Array(10).fill(null).map(() => createRandomAnnouncement());
+
+
+
+console.log(randomAnnouncements);
