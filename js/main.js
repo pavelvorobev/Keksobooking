7fcc22ffbb4f6a500ad4839cbc193a2f51d392e6
Pavel Vorobev <pavel_9_5@mail.ru>
2021-02-13 18:34:00 +0300

    Исправляет ошибки линтера

--- js/main.js
+++ js/main.js
@@ -35,8 +35,8 @@ const CHECKIN_TIME = ['12:00', '13:00', '14:00'];
 const CHECKOUT_TIME = ['12:00', '13:00', '14:00'];
 const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 const PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-                'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-                'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 const getRandomArrayElement = (array) => {
   return array[getRandomRoundedInt(0, array.length - 1)];
 };
